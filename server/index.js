70088fd886d0c98e125daf15b3373fc82a565d55
Sarani Mendis <saranimendisb@gmail.com>
2026-04-21 15:12:44 +0530

    Fix review comments

--- server/index.js
+++ server/index.js
@@ -9,16 +9,24 @@ const PORT = Number(process.env.PORT) || 3000;
 const HOST = '0.0.0.0';
 
 const MIME_TYPES = {
+  '.avif': 'image/avif',
   '.css': 'text/css; charset=utf-8',
+  '.eot': 'application/vnd.ms-fontobject',
+  '.gif': 'image/gif',
   '.html': 'text/html; charset=utf-8',
   '.ico': 'image/x-icon',
+  '.jpeg': 'image/jpeg',
+  '.jpg': 'image/jpeg',
   '.js': 'application/javascript; charset=utf-8',
   '.json': 'application/json; charset=utf-8',
   '.map': 'application/json; charset=utf-8',
   '.png': 'image/png',
   '.svg': 'image/svg+xml',
+  '.ttf': 'font/ttf',
   '.txt': 'text/plain; charset=utf-8',
   '.webp': 'image/webp',
+  '.woff': 'font/woff',
+  '.woff2': 'font/woff2',
 };
 
 function send(res, statusCode, body, headers = {}) {
@@ -146,6 +154,12 @@ async function main() {
   const server = http.createServer((req, res) => {
     requestHandler(req, res).catch((error) => {
       console.error('Unhandled server error:', error);
+
+      if (res.headersSent) {
+        res.destroy(error);
+        return;
+      }
+
       send(res, 500, 'Internal Server Error', {
         'Content-Type': 'text/plain; charset=utf-8',
       });
